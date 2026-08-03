997a3473c946d43c1b4863e255ad89ac6f4b8c49
97 <Vibincariappa@gmail.com>
2026-08-03 11:37:27 +0530

    ContainsDuplicate

---
+++ ContainsDuplicate.py
@@ -0,0 +1,31 @@
+#containsduplicate - Psuedocode
+
+# class Solution:
+#     def ContainsDuplicate(self,nums):
+#         n = len(nums) #determines the length of the arrray 
+
+#         for i in range(n): #iterates through the array -> nums = [1,2,3,1]
+#             for j in range(i+1,n): #iterates through the array starting from the next index of i
+#                 if nums[i] == nums[j]: #checks if element at index i is equal to elemnet in index j
+#                     return True
+#         return False
+
+#ContainsDuplicate - Using HashSEt
+
+class Solution:
+    def ContainsDuplicate(self,nums):
+        seen = set() #creates a set to store unique elements
+
+        for num in nums: #iterates through the array
+            if num in seen: #checks if the element is already in the set
+                return True #if it is, returns True
+            seen.add(num) #if not, adds the element to the set
+        return False #if no duplicates are found, returns False
+
+
+
+
+nums = list(map(int, input("Enter Numbers: ").split()))
+sol = Solution()
+print(sol.ContainsDuplicate(nums))
+
