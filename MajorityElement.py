3ec315eff85cd324cd03874e3777abbbbc24c5d4
97 <Vibincariappa@gmail.com>
2026-08-04 00:36:32 +0530

    MajorityElemen

---
+++ MajorityElement.py
@@ -0,0 +1,27 @@
+class Solution:
+    def MajorityElements(self,nums):
+
+#BruteForce
+        # for num in nums:
+        #     count = 0
+        #     n = len(nums)
+
+        #     for value in nums:
+        #         if value == num:
+        #             count += 1
+        #     if count > n // 2:
+        #         return num
+
+
+#Hashmap
+        count = {}
+
+        for num in nums:
+            count[num] = count.get(num,0)+1
+
+        for num,freq in count.items():
+            if freq > len(nums) // 2:
+                return num
+
+sol = Solution()
+print(sol.MajorityElements([3,2,3]))
