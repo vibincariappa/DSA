242e5cf4b7464a6183f6b970a0c631ba9d8ccfa7
97 <Vibincariappa@gmail.com>
2026-08-04 15:43:05 +0530

    GroupAnagram

--- GroupAnagram.py
+++ GroupAnagram.py
@@ -1,41 +1,66 @@
+from collections import defaultdict
 class Solution:
-    def isAnagram(self,s,t):
-        if len(s)!=len(t):
-            return False
 
-        t_list = list(t)
+#Brute Force
+    # def isAnagram(self,s,t):
+    #     if len(s)!=len(t):
+    #         return False
 
-        for ch in s:
-            found = False
+    #     t_list = list(t)
+
+    #     for ch in s:
+    #         found = False
         
-            for i in range(len(t_list)):
-                if t_list[i] == ch:
-                    t_list[i] = None
-                    found = True
-                    break
+    #         for i in range(len(t_list)):
+    #             if t_list[i] == ch:
+    #                 t_list[i] = None
+    #                 found = True
+    #                 break
                 
-            if not found:
-                return False
+    #         if not found:
+    #             return False
 
-        return True
+    #     return True
+
+    # def GroupAnagram(self,strs):
+    #     result = []
+    #     visited = [False]*len(strs)
+
+    #     for i in range(len(strs)):
+    #         if visited[i]:
+    #             continue
+
+    #         group = [strs[i]]
+    #         visited[i] = True
+        
+    #         for j in range(i+1,len(strs)):
+    #             if not visited[j] and self.isAnagram(strs[i], strs[j]):
+    #                 group.append(strs[j])
+    #                 visited[j] = True
+    #         result.append(group)
+    #     return result
+
+#USing HashMap
+    # def GroupAnagram(self,strs):
+    #     group = default(list)
+
+    #     for word in strs:
+    #         key = "".join(sorted(word))
+    #         group[key].append(word)
+
+    #     return list(group.values())
 
     def GroupAnagram(self,strs):
-        result = []
-        visited = [False]*len(strs)
+        group = defaultdict(list)
 
-        for i in range(len(strs)):
-            if visited[i]:
-                continue
+        for word in strs:
+            count = [0]*26
 
-            group = [strs[i]]
-            visited[i] = True
-        
-            for j in range(i+1,len(strs)):
-                if not visited[j] and self.isAnagram(strs[i], strs[j]):
-                    group.append(strs[j])
-                    visited[j] = True
-            result.append(group)
-        return result
+        for ch in word:
+            count[ord(ch) - ord('a')] += 1
+            group[tuple(count)].append(word)
+
+        return list(group.values())
 
 words = ["eat", "tea", "tan", "ate", "nat", "bat"]
 
